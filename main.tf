1462b14aefeb919bdc36e829ee85afbd531c304c
dircksens <marianadircksen13@gmail.com>
2024-07-02 21:15:06 -0300

    first commit

---
+++ main.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.27"
+    }
+  }
+
+  required_version = ">= 0.14.9"
+}
+
+provider "aws" {
+  profile = "default"
+  region  = "us-west-2"
+}
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-0cf2b4e024cdb6960"
+  instance_type = "t2.micro"
+  key_name = "projeto-final"
+  tags = {
+    Name = "Primeira instância"
+  }
+}
