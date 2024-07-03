d4a290ec5ab810080ab23ce19dcbcccd0c0fc2b1
dircksens <marianadircksen13@gmail.com>
2024-07-02 21:53:43 -0300

    .

--- main.tf
+++ main.tf
@@ -18,11 +18,6 @@ resource "aws_instance" "app_server" {
   ami           = "ami-0cf2b4e024cdb6960"
   instance_type = "t2.micro"
   key_name = "projeto-final"
-  user_data = <<-E0F 
-                #!/bin/bash 
-                cd /home/ubuntu 
-                echo "<h1>Feito com Terraform<h1>" > index.html 
-                nohup busybox httpd -f -p 8080 & EOF
   tags = {
     Name = "Primeira instância"
   }
